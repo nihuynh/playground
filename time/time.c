28351388a572719dbf40d6f074a60ebeae09a818
Nicolas Huynh <nico.huynh@gmail.com>
2019-03-15 02:21:03 +0100

    better timer

---
+++ time/time.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   time.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nihuynh <nihuynh@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/15 02:07:25 by nihuynh           #+#    #+#             */
+/*   Updated: 2019/03/15 02:20:38 by nihuynh          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <sys/time.h>
+#include <stdio.h>
+
+static inline long get_current_usec() {
+    struct timeval time;
+    gettimeofday(&time, NULL);
+    return (time.tv_sec * 1000 * 1000 + time.tv_usec);
+}
+
+int main() {
+	int		i;
+	long	elapsed_time;
+
+	elapsed_time = get_current_usec();
+	i = -1;
+	while(++i < 100000000)
+	{
+		i--;
+		i++;
+	}
+	elapsed_time = get_current_usec() - elapsed_time;
+	printf("It took %ld useconds to complete the task.\n", elapsed_time);
+	return (0);
+}
