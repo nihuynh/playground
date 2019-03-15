519080f78437f1b60adb2284a766b97d03497362
Nicolas Huynh <nico.huynh@gmail.com>
2019-03-15 01:07:10 +0100

    move livetime to libft

--- attribute/leaks.c
+++ attribute/leaks.c
@@ -6,7 +6,7 @@
 /*   By: nihuynh <nihuynh@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/28 14:22:39 by nihuynh           #+#    #+#             */
-/*   Updated: 2019/01/30 19:09:34 by nihuynh          ###   ########.fr       */
+/*   Updated: 2019/03/15 01:06:03 by nihuynh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,9 @@
 # include <string.h>
 # include <stdlib.h>
 
-__attribute__((destructor))
+// __attribute__((destructor))
 void clean(void)
 {
-	//free(&data);
 	printf("Clean-up\n");
 	while (1)
 		;
@@ -29,12 +28,13 @@ int remain(void)
 	data = strdup("Hello!\n");
 	printf("%s", data);
 	printf("How are you?\n");
-	//exit(EXIT_FAILURE);
+	exit(EXIT_FAILURE);
 	return (0);
 }
 
 int main(void)
 {
+	atexit(clean);
 	remain();
 	return (0);
 }
--- lldb.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-	int x=3;
-	char *name="bob";
-	float y=4.3f;
-	printf("Hello World\n");
-}
