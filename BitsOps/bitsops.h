c885ca250d67e8b1ab1dcda19e02d9db655afb96
Nicolas Huynh <nico.huynh@gmail.com>
2019-03-23 17:39:54 +0100

    half done on the bits. check it @cpireyre

---
+++ BitsOps/bitsops.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bitsops.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nihuynh <nihuynh@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/23 16:07:51 by nihuynh           #+#    #+#             */
+/*   Updated: 2019/03/23 17:21:39 by nihuynh          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITSOPS_H
+# define BITSOPS_H
+
+#include <stdio.h>
+
+typedef enum {ERROR = -1, FALSE, TRUE} LOGICAL;
+
+#define BOOL(x) (!(!(x)))
+
+// Bool of the value of the nth bit :
+# define BIT_OP_TEST(data, n) BOOL(data & (1UL << (n)))
+
+// Set the value of the nth bit to 1 :
+# define BIT_OP_SET(data, n) (data |= (1UL << n))
+
+// Set the value of the nth bit to 0 :
+# define BIT_OP_CLR(data, n) (data &= ~(1UL << n))
+
+// Set the value of the nth bit to val :
+# define BIT_OP_ASSIGN(data, n, val) (data & ~(1UL << n)) | (val << n)
+
+// Flip the value of the nth bit :
+# define BIT_OP_TOGGLE(data, n) (data ^= (1UL << n))
+
+// Add 1 to the nth bit :
+# define BIT_OP_ADD(data, n) (data &= (1UL << n))
+
+// Print bits
+# define BIT_OP_PRINT(data) for(size_t i = (1 << ((sizeof(data) * 8) - 1)); i > 0; i >>= 1) {putc((data & i) ? '1' : '0', stdout);};putc('\n', stdout)
+
+#endif
---
+++ BitsOps/test.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nihuynh <nihuynh@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/23 16:51:27 by nihuynh           #+#    #+#             */
+/*   Updated: 2019/03/23 17:37:55 by nihuynh          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "bitsops.h"
+
+#define TYPE_TESTED char
+
+void	test_test()
+{
+	TYPE_TESTED 	data;
+	int				len;
+
+	data = 0;
+	len = sizeof(data) * 8;
+	printf("%s has started\n", __func__);
+	while (--len >= 0)
+	{
+		if (BIT_OP_TEST(data, len))
+			return ;
+	}
+	printf("%s is a success\n", __func__);
+}
+
+void	test_set()
+{
+	TYPE_TESTED 	data;
+	TYPE_TESTED 	res;
+	int				len;
+
+	data = 0;
+	res = ~(data);
+	len = sizeof(data) * 8;
+	printf("%s has started\n", __func__);
+	while (--len >= 0)
+	{
+		BIT_OP_SET(data, len);
+	}
+	if (data == res)
+		printf("%s is a success\n", __func__);
+}
+
+void	test_clr()
+{
+	printf("%s has started\n", __func__);
+	// Do test
+		return ;
+	printf("%s is a success\n", __func__);
+}
+
+void	test_assign()
+{
+	printf("%s has started\n", __func__);
+	// Do test
+		return ;
+	printf("%s is a success\n", __func__);
+}
+void	test_toggle()
+{
+	printf("%s has started\n", __func__);
+	// Do test
+		return ;
+	printf("%s is a success\n", __func__);
+}
+
+void	test_add()
+{
+	printf("%s has started\n", __func__);
+	// Do test
+		return ;
+	printf("%s is a success\n", __func__);
+}
+
+
+int main (void)
+{
+	test_test();
+	test_set();
+	test_clr();
+	test_assign();
+	test_toggle();
+	test_add();
+	return (0);
+}
