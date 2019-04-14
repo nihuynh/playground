3b13af7e317f3676d81b2111f47ccf7714ca8117
Nicolas Huynh <nico.huynh@gmail.com>
2019-04-14 03:18:49 +0200

    cpu usage

---
+++ cputime/main.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nihuynh <nihuynh@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/14 02:05:55 by nihuynh           #+#    #+#             */
+/*   Updated: 2019/04/14 02:23:35 by nihuynh          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <mach/mach_init.h>
+#include <mach/mach_error.h>
+#include <mach/mach_host.h>
+#include <mach/vm_map.h>
+
+static unsigned long long _previousTotalTicks = 0;
+static unsigned long long _previousIdleTicks = 0;
+
+// Returns 1.0f for "CPU fully pinned", 0.0f for "CPU idle", or somewhere in between
+// You'll need to call this at regular intervals, since it measures the load between
+// the previous call and the current one.
+float CalculateCPULoad(unsigned long long idleTicks, unsigned long long totalTicks)
+{
+  unsigned long long totalTicksSinceLastTime = totalTicks-_previousTotalTicks;
+  unsigned long long idleTicksSinceLastTime  = idleTicks-_previousIdleTicks;
+  float ret = 1.0f-((totalTicksSinceLastTime > 0) ? ((float)idleTicksSinceLastTime)/totalTicksSinceLastTime : 0);
+  _previousTotalTicks = totalTicks;
+  _previousIdleTicks  = idleTicks;
+  return ret;
+}
+
+float GetCPULoad()
+{
+   host_cpu_load_info_data_t cpuinfo;
+   mach_msg_type_number_t count = HOST_CPU_LOAD_INFO_COUNT;
+   if (host_statistics(mach_host_self(), HOST_CPU_LOAD_INFO, (host_info_t)&cpuinfo, &count) == KERN_SUCCESS)
+   {
+      unsigned long long totalTicks = 0;
+      for(int i=0; i<CPU_STATE_MAX; i++) totalTicks += cpuinfo.cpu_ticks[i];
+      return CalculateCPULoad(cpuinfo.cpu_ticks[CPU_STATE_IDLE], totalTicks);
+   }
+   else return -1.0f;
+}
+
+
+int main (void)
+{
+	while (1)
+	{
+		printf("Cpu usage : %f\n", 100 * GetCPULoad());
+		sleep(1);
+	}
+}
