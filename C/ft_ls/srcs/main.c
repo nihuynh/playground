2fba8fc93a920a65981f86230c3cf452c5cd8ace
Nicolas Huynh <nico.huynh@gmail.com>
2019-07-22 13:08:03 +0200

    Update main.c

--- C/ft_ls/srcs/main.c
+++ C/ft_ls/srcs/main.c
@@ -6,18 +6,32 @@
 /*   By: nihuynh <nihuynh@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/22 11:28:26 by nihuynh           #+#    #+#             */
-/*   Updated: 2019/07/22 11:34:36 by nihuynh          ###   ########.fr       */
+/*   Updated: 2019/07/22 12:34:02 by nihuynh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
 #include "libft.h"
+
+int	ft_ls(char *path)
+{
+	struct dirent	*d;
+	DIR 			*dir;
+
+	if (!(dir = opendir(path)))
+		return (EXIT_FAILURE);
+	while ((d = readdir(dir))
+	{
+		printf("%s\n", d->d_name);
+	}
+	return (EXIT_SUCCESS);
+}
 
 int main(int ac, char const *av[])
 {
-
-	(void)ac;
-	(void)av;
-	/* code */
-	return 0;
+	if (ac == 2)
+		return (ft_ls(av[1]));
+	return (EXIT_SUCCESS);
 }
