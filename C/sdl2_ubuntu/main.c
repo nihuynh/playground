067df2b6868b81141289cd314749dfca14280102
Nicolas Huynh <nico.huynh@gmail.com>
2019-10-14 12:14:21 +0200

    minimal sdl2

---
+++ C/sdl2_ubuntu/main.c
@@ -0,0 +1,30 @@
+
+#include <SDL2/SDL.h>
+#include "libft.h"
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    SDL_Window *window = NULL;
+    int statut = EXIT_FAILURE;
+
+    if(0 != SDL_Init(SDL_INIT_VIDEO))
+    {
+        ft_printf("Erreur SDL_Init : %s", SDL_GetError());
+        goto Quit;
+    }
+    window = SDL_CreateWindow("SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                              640, 480, SDL_WINDOW_SHOWN);
+    if(NULL == window)
+    {
+        ft_printf("Erreur SDL_CreateWindow : %s", SDL_GetError());
+        goto Quit;
+    }
+     
+    statut = EXIT_SUCCESS;
+    SDL_Delay(3000);
+    SDL_DestroyWindow(window);
+Quit:
+    SDL_Quit();
+    return statut;
+}
