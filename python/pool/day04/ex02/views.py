28b0f2ac095f93e96ece384abb5e2a4ba77684d2
Nicolas Huynh <nico.huynh@gmail.com>
2019-10-15 15:28:31 +0200

    ggwp

--- python/pool/day04/ex02/views.py
+++ python/pool/day04/ex02/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django import forms
+    
+class Form(forms.Form):
+    input = forms.DateField(help_text="Enter a date between now and 4 weeks (default 3).")
