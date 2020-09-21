95651311a2bded570e5b0ed252b7639cf1fc460a
Nico <nico.huynh@gmail.com>
2020-09-21 09:17:52 +0200

    clear CV folder

--- C/codingame/main.c
+++ C/codingame/main.c
@@ -1,37 +1,25 @@
-/* C code below */
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <stdio.h>
 
-#include <stdbool.h>
-
-typedef struct {
-    long coin2;
-    long bill5;
-    long bill10;
-} Change;
-
-bool optimal_change(long s, Change *c)
+int compare(const void *a, const void *b)
 {
-    c->bill10 = s / 10;
-    s -= c->bill10 * 10;
-    c->bill5 = s / 5;
-    s -= c->bill5 * 5;
-    c->coin2 = s / 2;
-    s -= c->coin2 * 2;
-    return (s == 0);
+    return (*(int*)a - *(int*)b);
 }
 
-int main(void)
+
+bool exists(int ints[], int count, int k)
 {
-	Change c;
-	long s = 43; // Change this value to perform other tests
-	optimal_change(s, &c);
+    int *res;
 
-	printf("Coin(s)  2€: %d\n", c.coin2);
-	printf("Bill(s)  5€: %d\n", c.bill5);
-	printf("Bill(s) 10€: %d\n", c.bill10);
+    res = NULL;
+    res = bsearch(&k, ints, count, sizeof(int), compare);
+    return (res != NULL);
+}
 
-	long result = c.coin2*2 + c.bill5*5 + c.bill10*10;
-	printf("\nChange given = %d\n", result);
+int main()
+{
+    int ints[] = { -9, 14, 37, 102 };
+    printf("%d\n", exists(ints, 4, 39)); // 1
+    printf("%d\n", exists(ints, 4, 36)); // 0
 }
