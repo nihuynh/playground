adda3ee3e114081d8f4c718f9016e4331c668d30
Nico <nico.huynh@gmail.com>
2021-02-16 20:35:55 +0100

    rip 42

--- C/codingame/main.c
+++ C/codingame/main.c
@@ -1,26 +1,60 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+int dist_zero(int n)
+{
+    if (n > 0)
+        return n;
+    return -n;
+}
+
+int compare(const void *a, const void *b)
+{
+    return (*(int*)a - *(int*)b);
+}
+
+int compute_closest_to_zero(int n, int ts[n])
+{
+	int i = 0;
+	int dist_neg = 2147483647;
+	int dist_pos = 2147483647;
+	if (n == 1)
+		return ts[0];
+    if (n == 0)
+		return 0;
+    // for (int idx = 0; idx <= n; idx++) {
+    //     fprintf(stderr,"%d  ", ts[idx]);
+    // }
+    // fprintf(stderr,"\nSort\n");
+	qsort(ts, n, sizeof(int), compare);
+    // for (int idx = 0; idx <= n; idx++) {
+    //     fprintf(stderr,"%d  ", ts[idx]);
+    // }
+    for (i = 0; i <= n; i++) {
+		if (ts[i] > 0)
+			break;
+        if (dist_zero(ts[i]) < dist_neg) {
+            dist_neg = dist_zero(ts[i]);
+        }
+    }
+	for (int j = i; j <= n; j++) {
+        if (ts[j] != 0 && dist_zero(ts[j]) < dist_pos) {
+            dist_pos = dist_zero(ts[j]);
+        }
+    }
+	if (dist_neg < dist_pos)
+		return -dist_neg;
+	return dist_pos;
+}
 
 int main()
 {
-	struct
-	{
-		int len;
-	} *pointer = NULL;
-
-	pointer->len = 10;
-	++pointer->len; // increments len
-
-	printf("%d\n", pointer->len);
-
-	struct user
-	{
-		char *username;
-		int id;
-	} users[] = {
-		{"admin", 0},
-		{"leonardo", 1}};
-
-	printf("%s", users[0].username);
-
-	return 0;
+    int n = 5;
+    int ts[] = {-15, -7, -9, -14, -12};
+    int solution = compute_closest_to_zero(n, ts);
+    printf("%d\n", solution);
+    return 0;
 }
---
+++ C/codingame/qsort.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int compare(const void *a, const void *b)
+{
+    return (*(int*)a - *(int*)b);
+
+	if( *(int*)a < *(int*)b )
+		return -1;
+	else if( *(int*)a == *(int*)b )
+		return 0;
+	else return 1;
+}
+
+int main()
+{
+    int ints[] = { -9, 14, 37, 102 };
+   printf("Before sorting the list is: \n");
+   for(int n = 0 ; n < 5; n++ ) {
+      printf("%d ", ints[n]);
+   }
+   qsort(ints, 5, sizeof(int), compare);
+   printf("\nAfter sorting the list is: \n");
+   for(int n = 0 ; n < 5; n++ ) {
+      printf("%d ", ints[n]);
+   }
+   return(0);
+}
+
+
+    for(int i = 0 ; i < 5; i++ ) {
+      printf("%d ", numbers[i]);
+   }
+   qsort(numbers, n, sizeof(int), compare);
+   printf("\nAfter sorting the list is: \n");
+   for(int i = 0 ; i < 5; i++ ) {
+      printf("%d ", numbers[i]);
+   }
